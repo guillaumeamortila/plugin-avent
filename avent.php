1be66b08a4ea08d09ea3d787c641325a9f20a7c0
guillaumeamortila <guillaume.amortila@eemi.com>
2016-11-27 23:28:43 +0100

    Ajout des fichiers

---
+++ avent.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package Calendrier Avent
+ * @version 1.0
+ */
+/*
+Plugin Name: Calendrier de l'Avent
+Plugin URI: http://wordpress.org/plugins/pop-up/
+Description: Affichage d'une surprise chaque jour de l'avent à l'arrivée sur le site.
+Author: Guillaume Amortila
+Version: 1.0
+Author URI: http://guillaume-amortila/
+Text Domain: wp_text
+*/
+
+function my_func_enqueue_scripts()
+{
+	wp_enqueue_style( 'style', plugins_url('avent/inc/style.css'));
+	wp_enqueue_script( 'script', plugins_url('avent/inc/script.js'), ['jquery'], true);
+}
+
+add_action( 'wp_enqueue_scripts', 'my_func_enqueue_scripts');
+
+
+?>
+
+<div id='popup-avent'>
+	<div id='popup-avent-block'>
+		<img class='pictkdo' id='kdoopen' src='http://p8.storage.canalblog.com/83/24/1271466/100419009_o.png' alt='<?php _e("Christmas Gift","wp_text") ?>'>
+		<img class='pictkdo' id='kdoclose' src='http://www.coodyssee.fr/wp-content/blogs.dir/1/files/2014/12/Cadeau.png' alt='<?php _e("Christmas Gift","wp_text") ?>'>
+		<img id='fond' src='http://chouky39.c.h.pic.centerblog.net/eb4ed31a.gif' alt='<?php _e("Christmas Gift","wp_text") ?>'>
+		<div id="txtavt">
+			<br>
+			<h3><?php _e("Avent is now","wp_text") ?></h3>
+			<p><?php _e("Be patient, Christmas is coming !","wp_text") ?></p>
+			<p id="phrase"><?php _e("There are only","wp_text") ?><span id="days2go"></span><?php _e("days before Jesus born","wp_text") ?></p>
+		</div>
+		<div id='popup-close'>x</div>
+	</div>
+</div>
+
+<?php
+
+function wp_trad_init() {
+	load_plugin_textdomain(
+		'wp_text',
+		false,
+		dirname( plugin_basename( __FILE__ ) ) . '/languages'
+	);
+}
+add_action( 'plugins_loaded', 'wp_trad_init' );
+
+
+
+
+
+
---
+++ avent/inc/script.js
@@ -0,0 +1,103 @@
+jQuery(document).ready(function(){
+
+	//if ((month == 11) && (date < 26))
+	if (month == 10){
+		checkCookieAvt();
+	}
+});
+
+
+var day = new Date().getDay();
+var date = new Date().getDate();
+var month = new Date().getMonth();
+
+var exp = new Date();
+//exp.setHours(23);
+//exp.setMinutes(59);
+//exp.setSeconds(59);
+exp.setSeconds(exp.getSeconds()+1);
+exp = exp.toUTCString();
+
+
+// COOKIE - ouverture unique
+
+function setCookie(cName, cValue){
+	document.cookie = cName+"="+cValue+";expires="+exp;
+}
+
+function getCookie(cName){
+	var name = cName + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookieAvt() {
+    var cVal=getCookie("ouverture_unique");
+    if (cVal == "") {
+    	popup();
+    	cVal = date;
+    	if (cVal != "" && cVal != null) {
+        	setCookie("ouverture_unique", cVal);
+       }
+    }
+}
+
+
+// POPUP
+
+$avt = jQuery('#popup-avent');
+$avt_block = jQuery('#popup-avent-block');
+$close = jQuery('#popup-close');
+$kdoclose = jQuery('#kdoclose');
+$kdoopen = jQuery('#kdoopen');
+$txtavt = jQuery('#txtavt');
+
+function fadeOutKdo(){
+    $kdoopen.css('transition','width 1s, left 1s');
+    $kdoopen.css('width','100%');
+    $kdoopen.css('left','-6%');
+    setTimeout($kdoopen.fadeOut(600),400);
+    setTimeout($txtavt.fadeIn(600),400);
+    setTimeout(jQuery('#fond').fadeIn(500),400);
+    jQuery('#fond').css('display','block');
+}
+
+function popup(){
+	$avt.css('display','flex');
+    var days2go = (25 - date);
+    jQuery("#days2go").append(' '+days2go+' ');
+
+    $avt.click(function(){
+        $avt.fadeOut();
+    });
+
+    $avt_block.click(function(event){
+        event.stopPropagation();
+    });
+
+    $close.click(function(){
+        $avt.fadeOut();
+    });
+
+    $kdoclose.click(function(){
+        $kdoclose.fadeOut();
+        $kdoopen.fadeIn();
+        setTimeout(fadeOutKdo, 200);
+    });
+
+}
+
+
+
+
+
+
